af85f11946ae3e2ad7170a21203f69caaa68bab8
sri-bhatta <70800017+sri-bhatta@users.noreply.github.com>
2021-08-25 13:38:10 +0530

    Add files via upload

---
+++ filtering.py
@@ -0,0 +1,158 @@
+import numpy as np
+
+
+def recordings_with_region(data, region):
+    # returns recording index that has a region and number of recorded neurons
+    recorded_with_region = []
+    for idx, dat in enumerate(data):
+        regions, count = np.unique(dat["brain_area"], return_counts=True)
+        if region in regions:
+            pos = np.where(dat["brain_area"] == region)[0]
+            recorded_with_region.append([idx, pos.shape[0]])
+
+    return np.vstack(recorded_with_region)
+
+
+def regions_recorded(data):
+    # returns all regions recorded
+    recorded_regions = np.array([])
+    for idx, dat in enumerate(data):
+        # recorded brain regions
+        regions, count = np.unique(dat["brain_area"], return_counts=True)
+        recorded_regions = np.hstack([regions, recorded_regions])
+
+    recorded_regions, animal_count = np.unique(recorded_regions, return_counts=True)
+    return list(zip(recorded_regions, animal_count))
+
+
+def regions_recorded_per_animal(data):
+    # returns regions recorded per animal
+    recorded_regions = []
+    for idx, dat in enumerate(data):
+        # recorded brain regions
+        regions, count = np.unique(dat["brain_area"], return_counts=True)
+        recorded_regions.append([idx, list(zip(regions, count))])
+
+    return np.vstack(recorded_regions)
+
+
+def get_times_for_neuron(neurons_spks, times, stim_onset=50):
+    # generates a neurons_spks-like array with 1 where times happens and 0 everywhere else
+    neurons_count = neurons_spks.shape[0]
+    trials_count = neurons_spks.shape[1]
+    neurons_responses = np.zeros_like(neurons_spks)
+    response_time_bins = np.asarray((times * 10) + stim_onset).ravel().astype(np.int)
+    for neuron in range(neurons_count):
+        for trial in range(trials_count):
+            neurons_responses[neuron][trial][response_time_bins[trial]] = 1
+    return neurons_responses
+
+
+def filter_contralateral_trials_full_contrast(data, regions, c_left=0, c_right=1, everything=False):
+    cl_trials_data = []
+    neuron_idxs = []
+    for dat in data:
+        region_filter_idx = np.where([(dat["brain_area"][idx] in regions) for idx, __ in enumerate(dat["brain_area"])])[0]
+        if any(region_filter_idx):
+            if everything is True:
+                contralateral_trials_idx = np.arange(len(dat["contrast_left"]))
+            else:
+                contralateral_trials_idx = np.where((dat["contrast_left"] == c_left) & (dat["contrast_right"] == c_right))[0]
+            neuron_idxs.append(contralateral_trials_idx)
+            mouse_name = dat["mouse_name"]
+            # filter neurons by regions
+            mouse_spikes = dat["spks"][region_filter_idx]
+            # filter neurons by contralateral trials
+            mouse_spikes = mouse_spikes[:, contralateral_trials_idx]
+            mouse_regions = dat["brain_area"][region_filter_idx]
+            mouse_gocue = dat["gocue"][contralateral_trials_idx]
+            mouse_resptime = dat["response_time"][contralateral_trials_idx]
+            mouse_wheel = dat["wheel"][0][contralateral_trials_idx]
+            mouse_feedback = dat["feedback_time"][contralateral_trials_idx]
+            mouse_right = dat["contrast_right"][contralateral_trials_idx]
+            mouse_left = dat["contrast_left"][contralateral_trials_idx]
+            mouse_response = dat["response"][contralateral_trials_idx]
+            mouse_reactiontime = dat["reaction_time"][contralateral_trials_idx]
+            cl_trials_data.append(
+                [
+                    mouse_name,
+                    mouse_spikes,
+                    mouse_regions,
+                    mouse_gocue,
+                    mouse_resptime,
+                    mouse_wheel,
+                    mouse_feedback,
+                    mouse_right,
+                    mouse_left,
+                    mouse_response,
+                    mouse_reactiontime,
+                ]
+            )
+    return np.asarray(cl_trials_data), np.asarray(neuron_idxs)
+
+def filter_trials_full_contrast(data, regions):
+    cl_trials_data = []
+    for dat in data:
+        region_filter_idx = np.where([(dat["brain_area"][idx] in regions) for idx, __ in enumerate(dat["brain_area"])])[0]
+        if any(region_filter_idx):
+            contralateral_trials_idx = np.arange(len(dat["contrast_left"]))
+            mouse_name = dat["mouse_name"]
+            # filter neurons by regions
+            mouse_spikes = dat["spks"][region_filter_idx]
+            # filter neurons by contralateral trials
+            mouse_spikes = mouse_spikes[:, contralateral_trials_idx]
+            mouse_regions = dat["brain_area"][region_filter_idx]
+            mouse_gocue = dat["gocue"][contralateral_trials_idx]
+            mouse_resptime = dat["response_time"][contralateral_trials_idx]
+            mouse_wheel = dat["wheel"][0][contralateral_trials_idx]
+            mouse_feedback = dat["feedback_time"][contralateral_trials_idx]
+            mouse_stim_left = dat["contrast_left"][contralateral_trials_idx]
+            mouse_stim_right = dat["contrast_right"][contralateral_trials_idx] 
+            mouse_response = dat["response"][contralateral_trials_idx]
+            cl_trials_data.append(
+                [
+                    mouse_name,
+                    mouse_spikes,
+                    mouse_regions,
+                    mouse_gocue,
+                    mouse_resptime,
+                    mouse_wheel,
+                    mouse_feedback,
+                    mouse_stim_left, 
+                    mouse_stim_right, 
+                    mouse_response
+                ]
+            )
+    return np.asarray(cl_trials_data)
+
+def filter_contralateral_by_region(data, region):
+    combined_data = []
+    # iterate recordings
+    for recording in data:
+        region_idx = np.where((recording[2] == region))[0]
+        if any(region_idx):
+            mouse_name = recording[0]
+            # filter neurons by regions
+            mouse_spikes = recording[1]
+            # filter neurons by region
+            mouse_spikes = mouse_spikes[region_idx]
+            mouse_regions = recording[2][region_idx]
+            mouse_gocue = recording[3]
+            mouse_resptime = recording[4]
+            mouse_wheel = recording[5]
+            mouse_feedback = recording[6]
+            mouse_response = recording[7]
+            combined_data.append(
+                [
+                    mouse_name,
+                    mouse_spikes,
+                    mouse_regions,
+                    mouse_gocue,
+                    mouse_resptime,
+                    mouse_wheel,
+                    mouse_feedback,
+                    mouse_response,
+                ]
+            )
+
+    return np.asarray(combined_data)
---
+++ plotting.py
@@ -0,0 +1,205 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.ndimage import gaussian_filter1d
+
+
+def save_or_plot(save_path, save):
+    if save:
+        plt.draw()
+        plt.savefig(save_path, bbox_inches="tight", pad_inches=0)
+        plt.close()
+    else:
+        plt.show()
+
+
+def plot_spiking_histogram_neurons_all_trials(neurons_spks, region, bins=50, save_path="./data.png", save=False):
+    neurons_count = neurons_spks.shape[0]
+    spks_per_trial = [np.sum(neurons_spks[neuron]) for neuron in range(neurons_count)]
+    plt.hist(spks_per_trial, bins=bins)
+    plt.xlabel("total number of spikes")
+    plt.ylabel("number of neurons")
+    plt.title(f"spiking activity; region: {region}")
+    save_or_plot(save_path, save)
+
+
+def plot_spiking_histogram_neurons_in_regions(data, regions, bins=50, save_path="./data.png", save=False):
+    for region in regions:
+        neurons = data["brain_area"] == region
+        neurons_spks = data["spks"][neurons]
+        neurons_count = neurons_spks.shape[0]
+        spks_per_trial = [np.sum(neurons_spks[neuron]) for neuron in range(neurons_count)]
+        plt.hist(spks_per_trial, bins=bins)
+        plt.title(f"region: {region}")
+        plt.xlabel("total number of spikes")
+        plt.ylabel("number of neurons")
+        save_or_plot(save_path, save)
+
+
+def plot_average_activity_neurons_all_trials(neurons_spks, region, sigma=2, sort=True, save_path="./data.png", save=False):
+    avg_activity = np.mean(neurons_spks, axis=1)
+    total_act = np.sum(avg_activity, axis=1)
+
+    fig = plt.figure(dpi=150)
+
+    # sort by total activity
+    if sort:
+        avg_acitivity_sorted = [x for _, x in sorted(zip(total_act, np.arange(0, avg_activity.shape[0])))]
+        plt.imshow(gaussian_filter1d(avg_activity[avg_acitivity_sorted], sigma, axis=1), cmap="gray_r", alpha=1)
+    else:
+        plt.imshow(avg_activity, cmap="gray_r", alpha=1)
+    #         plt.colorbar()
+    plt.axvline(50, color="limegreen", label="stimulus onset")
+    plt.title(f"avg activity; region: {region}; sigma: {sigma}")
+    plt.xlabel("10ms time bins")
+    plt.ylabel(f"neuron")
+    plt.legend()
+    save_or_plot(save_path, save)
+
+
+def plot_average_activity_neurons_in_regions(data, regions, sigma=2, sort=True, save_path="./data.png", save=False):
+    for region in regions:
+        neurons = data["brain_area"] == region
+        neurons_spks = data["spks"][neurons]
+
+        avg_activity = np.mean(neurons_spks, axis=1)
+        total_act = np.sum(avg_activity, axis=1)
+
+        fig = plt.figure(dpi=150)
+
+        # sort by total activity
+        if sort:
+            avg_acitivity_sorted = [x for _, x in sorted(zip(total_act, np.arange(0, avg_activity.shape[0])))]
+            plt.imshow(gaussian_filter1d(avg_activity[avg_acitivity_sorted], sigma, axis=1), cmap="gray_r", alpha=1)
+        else:
+            plt.imshow(gaussian_filter1d(avg_activity, sigma, axis=1), cmap="gray_r", alpha=1)
+        #         plt.colorbar()
+        plt.axvline(50, color="limegreen", label="stimulus onset")
+        plt.title(f"avg activity; region: {region}")
+        plt.xlabel("10ms time bins")
+        plt.ylabel(f"neuron")
+        plt.legend()
+        save_or_plot(save_path, save)
+
+
+def plot_per_trial_activity(neurons_spks, region, gocue=None, response_time=None, save_path="./data.png", save=False):
+    neurons_count = neurons_spks.shape[0]
+    for neuron in range(neurons_count):
+        fig = plt.figure(dpi=150)
+        plt.imshow(neurons_spks[neuron], cmap="gray_r", alpha=1)
+        plt.colorbar()
+        if gocue is not None:
+            plt.imshow(gocue[neuron], cmap="magma", alpha=0.25)
+        if response_time is not None:
+            plt.imshow(response_time[neuron], cmap="magma", alpha=0.25)
+        plt.axvline(50, color="limegreen", label="stimulus onset")
+        plt.xlabel("10ms time bins")
+        plt.ylabel(f"neuron {neuron}; trials")
+        plt.title(f"per trial activity; region: {region}")
+        plt.legend()
+        save_or_plot(save_path, save)
+
+
+def psth(neurons_spks, region, timebin_size=1, func=np.mean, sigma=3, save_path="./data.png", save=False):
+    total_activity = func(neurons_spks, axis=1)
+    total_activity = np.sum(total_activity, axis=0)
+
+    # combine bins according to timebin_size
+    tas = total_activity.shape[0]
+    total_activity = total_activity.reshape([tas // timebin_size, -1])
+    total_activity = np.sum(total_activity, axis=1)
+
+    fig = plt.figure(dpi=150)
+
+    plt.step(np.arange(total_activity.shape[0]), gaussian_filter1d(total_activity, sigma))
+    plt.axvline(50 // timebin_size, color="limegreen", label="stimulus onset")
+    plt.title(f"PSTH; region: {region}")
+    plt.xlabel("10ms time bins")
+    plt.ylabel(f"spike count")
+    plt.ylim([np.min(total_activity), np.max(total_activity) * 1.25])
+    plt.legend()
+    save_or_plot(save_path, save)
+
+
+def psth_combined(alldat, region, timebin_size=1, func=np.mean, sigma=3, save_path="./data.png", save=False):
+    fig = plt.figure(dpi=150)
+    tas_max = 0
+    mmax = 0
+    for dat in alldat:
+        neurons = dat["brain_area"] == region
+        neurons_spks = dat["spks"][neurons]
+        neurons_count = neurons_spks.shape[0]
+        total_neurons_count = dat["spks"].shape[0]
+        trials_count = dat["spks"].shape[1]
+        if neurons_count > 1:
+            total_activity = func(neurons_spks, axis=1)
+            total_activity = np.sum(total_activity, axis=0)
+            # combine bins according to timebin_size
+            tas = total_activity.shape[0]
+            total_activity = total_activity.reshape([tas // timebin_size, -1])
+            total_activity = np.sum(total_activity, axis=1)
+            tas_max = np.max(total_activity)
+
+            plt.step(np.arange(total_activity.shape[0]), gaussian_filter1d(total_activity, sigma), label=dat["mouse_name"])
+        mmax = tas_max if tas_max > mmax else mmax
+    plt.axvline(50 // timebin_size, color="limegreen", label="stimulus onset")
+    plt.title(f"PSTH; region: {region}")
+    plt.xlabel("10ms time bins")
+    plt.ylabel(f"spike count")
+    plt.ylim([0, mmax])
+    plt.legend()
+    save_or_plot(save_path, save)
+
+
+def psth_combined_from_filtered(alldat, region, timebin_size=1, func=np.mean, sigma=3, save_path="./data.png", save=False):
+    fig = plt.figure(dpi=150)
+    tas_max = 0
+    mmax = 0
+    for dat in alldat:
+        name, spks = dat
+        total_activity = func(spks, axis=1)
+        total_activity = np.sum(total_activity, axis=0)
+        # combine bins according to timebin_size
+        tas = total_activity.shape[0]
+        total_activity = total_activity.reshape([tas // timebin_size, -1])
+        total_activity = np.sum(total_activity, axis=1)
+        tas_max = np.max(total_activity)
+
+        plt.step(np.arange(total_activity.shape[0]), gaussian_filter1d(total_activity, sigma), label=name)
+        mmax = tas_max if tas_max > mmax else mmax
+    plt.axvline(50 // timebin_size, color="limegreen", label="stimulus onset")
+    plt.title(f"PSTH; region: {region}")
+    plt.xlabel("10ms time bins")
+    plt.ylabel(f"spike count")
+    plt.ylim([0, mmax])
+    plt.legend()
+    save_or_plot(save_path, save)
+
+
+def plot_spiking_activity_from_filtered(neurons_spks, region, sort=True, save_path="./data.png", save=False):
+    combined_activity = np.vstack(neurons_spks)
+    total_act = np.sum(combined_activity, axis=1)
+
+    fig = plt.figure(figsize=(240, 60), dpi=175)
+
+    # sort by total activity
+    if sort:
+        combined_activity_sorted = [x for _, x in sorted(zip(total_act, np.arange(0, combined_activity.shape[0])))]
+        plt.imshow(combined_activity[combined_activity_sorted], cmap="gray_r", alpha=1)
+    else:
+        plt.imshow(combined_activity, cmap="gray_r", alpha=1)
+    plt.axvline(50, color="limegreen", label="stimulus onset")
+    plt.title(f"avg activity; region: {region}")
+    plt.xlabel("10ms time bins")
+    plt.ylabel(f"neuron")
+    plt.legend()
+    save_or_plot(save_path, save)
+
+
+def plot_wheel_movement(trial, wheel_to_mm=0.135, sigma=3, title="wheel position x time", save_path="./data.png", save=False):
+    plt.plot(gaussian_filter1d(np.cumsum(trial * wheel_to_mm), 3))
+    plt.axvline(50, color="limegreen", label="stimulus onset")
+    plt.xlabel("10ms time bins")
+    plt.ylabel(f"wheel position (mm)")
+    plt.title(title)
+    plt.legend()
+    save_or_plot(save_path, save)
